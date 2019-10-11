98ff96b09d95f9d2880e7845f62e5cba4b5adb8e
MTProto <52460888+MTProto@users.noreply.github.com>
2019-10-11 19:04:40 +0330

    Added support for Fake TLS
    Added support for Fake TLS, API changed as well.

--- mtprotoproxy.js
+++ mtprotoproxy.js
@@ -1,19 +1,75 @@
 'use strict'
 
 const net = require('net');
+const https =require('https');
 const crypto = require('crypto');
-const EventEmitter = require('events');
 
-const rp = require('request-promise'); 
-const { crc32 } = require('crc');
+function rp(url)
+{
+	return new Promise(function(accept,reject)
+	{
+		let rawData=[];
+		let c=https.get(url,function(res)
+		{
+			if (res.statusCode!==200)
+				reject(new Error(`Status code is ${res.statusCode}`));
+			res.on('data',function(ret)
+			{
+				rawData.push(ret);
+			})
+			res.on('end',function()
+			{
+				accept(Buffer.concat(rawData));
+			})
+		})
+		c.on('error',reject);
+	})
+}
 
+let TABLE=new Uint32Array(256)
+Buffer.from([
+'AAAAAJYwB3csYQ7uulEJmRnEbQeP9GpwNaVj6aOVZJ4yiNsOpLjceR7p1eCI2dKXK0y2Cb18sX4HLbjn',
+'kR2/kGQQtx3yILBqSHG5895BvoR91Noa6+TdbVG11PTHhdODVphsE8Coa2R6+WL97Mllik9cARTZbAZj',
+'Yz0P+vUNCI3IIG47XhBpTORBYNVycWei0eQDPEfUBEv9hQ3Sa7UKpfqotTVsmLJC1sm720D5vKzjbNgy',
+'dVzfRc8N1txZPdGrrDDZJjoA3lGAUdfIFmHQv7X0tCEjxLNWmZW6zw+lvbieuAIoCIgFX7LZDMYk6Qux',
+'h3xvLxFMaFirHWHBPS1mtpBB3HYGcdsBvCDSmCoQ1e+JhbFxH7W2BqXkv58z1LjooskHeDT5AA+OqAmW',
+'GJgO4bsNan8tPW0Il2xkkQFcY+b0UWtrYmFsHNgwZYVOAGLy7ZUGbHulARvB9AiCV8QP9cbZsGVQ6bcS',
+'6ri+i3yIufzfHd1iSS3aFfN804xlTNT7WGGyTc5RtTp0ALyj4jC71EGl30rXldg9bcTRpPv01tNq6WlD',
+'/NluNEaIZ63QuGDacy0EROUdAzNfTAqqyXwN3TxxBVCqQQInEBALvoYgDMkltWhXs4VvIAnUZrmf5GHO',
+'DvneXpjJ2SkimNCwtKjXxxc9s1mBDbQuO1y9t61susAgg7jttrO/mgzitgOa0rF0OUfV6q930p0VJtsE',
+'gxbccxILY+OEO2SUPmptDahaanoLzw7knf8JkyeuAAqxngd9RJMP8NKjCIdo8gEe/sIGaV1XYvfLZ2WA',
+'cTZsGecGa252G9T+4CvTiVp62hDMSt1nb9+5+fnvvo5DvrcX1Y6wYOij1tZ+k9GhxMLYOFLy30/xZ7vR',
+'Z1e8pt0GtT9LNrJI2isN2EwbCq/2SgM2YHoEQcPvYN9V32eo745uMXm+aUaMs2HLGoNmvKDSbyU24mhS',
+'lXcMzANHC7u5FgIiLyYFVb47usUoC72yklq0KwRqs1yn/9fCMc/QtYue2Swdrt5bsMJkmybyY+yco2p1',
+'CpNtAqkGCZw/Ng7rhWcHchNXAAWCSr+VFHq44q4rsXs4G7YMm47Skg2+1eW379x8Id/bC9TS04ZC4tTx',
+'+LPdaG6D2h/NFr6BWya59uF3sG93R7cY5loIiHBqD//KOwZmXAsBEf+eZY9prmL40/9rYUXPbBZ44gqg',
+'7tIN11SDBE7CswM5YSZnp/cWYNBNR2lJ23duPkpq0a7cWtbZZgvfQPA72DdTrrypxZ673n/Pskfp/7Uw',
+'HPK9vYrCusowk7NTpqO0JAU20LqTBtfNKVfeVL9n2SMuemazuEphxAIbaF2UK28qN74LtKGODMMb3wVa',
+'je8CLQ=='].join(),'base64').copy(Buffer.from(TABLE.buffer));
+
+function crc32(buf, previous) {
+  let crc = ~~previous ^ -1;
+  for (let index = 0; index < buf.length; index++) {
+    const byte = buf[index];
+    crc = TABLE[(crc ^ byte) & 0xff] ^ (crc >>> 8);
+  }
+  return crc ^ -1
+}
+
+
+
+const TLS_START_BYTES = Buffer.from('1603010200010001fc0303','hex');
+const MAX_CHUNK_SIZE = 16384 + 24;
+
+const firstPart =Buffer.from('1603030000020000000303000000000000000000000000000000000000000000000000000000000000000000','hex');
+const secondPart=Buffer.from('130100002e00330024001d00200000000000000000000000000000000000000000000000000000000000000000002b000203041403030001011703030000','hex')
 const PROXY_INFO_ADDR   = 'https://core.telegram.org/getProxyConfig';
 const PROXY_SECRET_ADDR = 'https://core.telegram.org/getProxySecret';
 const PUBLIC_IP_ADDR = 'https://api.ipify.org/';
 
 let globaleIndex=0;
-let secrets;
-let AD_TAG;
+let secured_secrets,tls_secrets;
+//let AD_TAG;
 const CON_TIMEOUT= 5 * 60 * 1000;
 
 let PROXY_SECRET;
@@ -58,7 +114,7 @@ function refreshProxyInfo()
 {
 	return Promise.all([PROXY_INFO_ADDR,PROXY_SECRET_ADDR,PUBLIC_IP_ADDR].map(function(url)
 	{
-		return rp({url,encoding:null})
+		return rp(url)
 	})).then(function(arr)
 	{
 		PROXY_SECRET=arr[1];
@@ -232,79 +288,91 @@ let extendedSocket=
 	startBuffering(name)
 	{
 		let self=this;
+		self.autoWrite=true;
+		self.setOutputBufferLength=function(){};
+		self.outputBuffers=[];
 		self.localName=name;
-		self.writeCRCenabled=false;
-		self.readCRCenabled=false;
-		self.writeCRCval=undefined;
-		self.readCRCval=undefined;
+		self.writeCRCval=0x00000000;
+		self.readCRCval=0x00000000;
 		self.newDataCallback=function(){};
 		self.errorCallback=function(){};
 		self.inputBuffer=Buffer.alloc(0);
+		self.cryptoLayers=[];
 		self.on('data',function(data){self.ondata.call(self,data)}); //why
 		self.on('timeout',function(){self.emit('finished',new Error(`${self.localName} timedout`))})
 		self.on('end'  ,function()		{self.emit('finished',new Error(`${self.localName} ended`))});
 		self.on('error',function(err)	{self.emit('finished',err)});
 		self.once('finished',function(err){self.end();self.errorCallback(err);self.setErrorHandler=function(handler){throw err}})
 	},
-	addCryptoLayer({decoder,encoder})
+	addCryptoLayer(layer)
 	{
+		this.cryptoLayers.push(layer);
+		let {decoder}=layer
 		if (this.inputBuffer.length>0)
 			this.inputBuffer=decoder(this.inputBuffer);
-		let oldondata=this.ondata;
-		this.ondata=function(data){oldondata.call(this,decoder(data))};
-		let oldPusher=this.pusher;
-		this.pusher=function(data){oldPusher.call(this,encoder(data))};
 	},
 	setErrorHandler(handler)
 	{
 		if (handler)
 			this.errorCallback=handler;
 	},
-	pusher(data)
-	{
-		if (data.length>0)
-			this.write(data)
-	},
 	bufferedWrite(data)
 	{
-		if (this.writeCRCenabled)
-			this.writeCRCval = crc32(data,this.writeCRCval);
-		this.pusher(data);
+		if (data.length===0)
+			return
+		this.writeCRCval = crc32(data,this.writeCRCval);
+		for (let layer of this.cryptoLayers.slice().reverse())
+			data=layer.encoder(data);
+		if (data.length!==0)
+		{
+			if (this.autoWrite)
+				this.write(data)
+			else
+				this.outputBuffers.push(data);
+		}
+	},
+	flushBuffer()
+	{
+		this.write(Buffer.concat(this.outputBuffers));
+		this.outputBuffers=[];
 	},
 	beginWriteCRC()
 	{
-		this.writeCRCenabled=true;
-		this.writeCRCval=undefined;
+		this.writeCRCval=0x00000000;
 	},
 	endWriteCRC()
 	{
-		this.writeCRCenabled=false;
 		let ret=Buffer.alloc(4);
-		ret.writeUInt32LE(this.writeCRCval);
-		this.writeCRCval=undefined;
+		ret.writeInt32LE(this.writeCRCval);
+		this.writeCRCval=0x00000000;
 		this.bufferedWrite(ret);
 	},
 	beginReadCRC()
 	{
-		this.readCRCval=undefined;
-		this.readCRCenabled=true;
+		this.readCRCval=0x00000000;
 	},
 	async endReadCRC()
 	{
-		this.readCRCenabled=false;
 		let ret=Buffer.alloc(4);
-		ret.writeUInt32LE(this.readCRCval);
-		this.readCRCval=undefined;
+		ret.writeInt32LE(this.readCRCval);
+		this.readCRCval=0x00000000;
 		return this.bufferedReadAssert(ret)
 	},
 	ondata(data)
 	{
+		for (let layer of this.cryptoLayers)
+			data=layer.decoder(data);
+
+		if (data.length===0)
+			return
 		this.inputBuffer=Buffer.concat([this.inputBuffer,data]);
 		this.newDataCallback();
 	},
 	waitForNewData()
 	{
 		let self=this;
+		if (self.idling)
+			self.idling();
 		return new Promise(function(accept,reject)
 		{
 			self.newDataCallback=accept;
@@ -320,8 +388,7 @@ let extendedSocket=
 		}
 		data=this.inputBuffer;
 		this.inputBuffer=Buffer.alloc(0);
-		if (this.readCRCenabled)
-			this.readCRCval=crc32(data,this.readCRCval)
+		this.readCRCval=crc32(data,this.readCRCval)
 		return data;
 	},
 	async bufferedReadExactly(len)
@@ -333,8 +400,7 @@ let extendedSocket=
 		}
 		data=this.inputBuffer.slice(0,len);
 		this.inputBuffer=this.inputBuffer.slice(len);
-		if (this.readCRCenabled)
-			this.readCRCval=crc32(data,this.readCRCval)
+		this.readCRCval=crc32(data,this.readCRCval)
 		return data;
 	},
 	async bufferedReadAtmost(len)
@@ -347,8 +413,7 @@ let extendedSocket=
 		len=Math.min(len,this.inputBuffer.length);
 		data=this.inputBuffer.slice(0,len);
 		this.inputBuffer=this.inputBuffer.slice(len);
-		if (this.readCRCenabled)
-			this.readCRCval=crc32(data,this.readCRCval)
+		this.readCRCval=crc32(data,this.readCRCval)
 		return data;
 	},
 	async bufferedReadInt()
@@ -409,28 +474,172 @@ function createAESCTRTransform({key,iv},mode)
 	return ((data)=>cr.update(data));
 }
 
-async function handleClient(client)
+
+async function handleClient(client,filter,id)
 {
+	let currentSecret;
+	let isTLS=false;
+	let AD_TAG
 	client.setKeepAlive(true);
-	client.setTimeout(CON_TIMEOUT);
+	client.setNoDelay();
+	client.setTimeout(5000);
 	let cl_ip 	=Buffer.from(client.remoteAddress.split('.'));
 	let cl_port =client.remotePort;
 	extendSocket(client,'client');
-	let skip = await client.bufferedReadExactly(8);
-	let dec_prekey_and_iv = await client.bufferedReadExactly(48);
-	let dec_proto_tag = await client.bufferedReadExactly(4);
-	let dec_dcId = await client.bufferedReadExactly(2);
-	let trailing = await client.bufferedReadExactly(2);
+	let h1=await client.bufferedReadExactly(64);
+	if (h1.compare(TLS_START_BYTES,0,11,0,11)===0)
+	{
+		isTLS=true
+		//await client.bufferedReadAssert(TLS_START_BYTES);
+		let h2=await client.bufferedReadExactly(453);
+		let handshake=Buffer.concat([h1,h2]);
+		let sni_length=handshake.readUInt8(128);
+		let SNI=handshake.slice(129,129+sni_length).toString();
+		let d=handshake.slice(11, 11 + 32);
+		let digest=Buffer.from(d);
+		d.fill(0);
+		let isOK=false;
+		let mainPacket;
+		for (let secret of tls_secrets)
+		{
+			let computed_digest=crypto.createHmac('sha256', secret.secret).update(handshake).digest();
+			if (computed_digest.compare(digest,0,28,0,28)!==0)
+				continue
+			let timestamp=computed_digest.readUInt32LE(28)^digest.readUInt32LE(28);
+			let lag=timestamp-Math.floor(Date.now()/1000);
+			if (Math.abs(lag)>60*10)
+				continue
+
+			let sess_id_len = handshake[11];
+	    	let sess_id = handshake.slice(11, 11 + sess_id_len);
+			let fake_cert_len=1024+Math.floor(3*1024*Math.random());
+			let http_data = crypto.randomBytes(fake_cert_len);
+
+	    	let p1=Buffer.from(firstPart);
+	    	let p2=Buffer.from(secondPart);
+	    	p1.writeUInt16BE(90+sess_id_len,3);
+	    	p1.writeUInt32BE(86+sess_id_len,5);
+	    	p1.writeUInt8(2,5);
+	    	p1.writeUInt8(sess_id_len,43);
+	    	crypto.randomBytes(32).copy(p2,13); //x25519_public_key
+	    	p2.writeUInt16BE(fake_cert_len,60);
+	    	let dd=Buffer.concat([digest,p1,sess_id,p2,http_data]);
+	    	mainPacket=dd.slice(32);
+	    	let digestPlaceHolder=mainPacket.slice(11);
+	    	crypto.createHmac('sha256', secret.secret).update(dd).digest().copy(digestPlaceHolder)
+
+	        isOK=true;
+	        currentSecret=secret;
+	        break;
+		}
+		
+
+		assertit(isOK,'No matching secret found')
+		AD_TAG=await filter({id,address:client.remoteAddress,port:client.remotePort,secretIndex:currentSecret.index,SNI});
+		client.bufferedWrite(mainPacket)
+		client.addCryptoLayer(
+			(function()
+			{
+				let cache=Buffer.alloc(0);
+				let remainingLen1=0;
+				let remainingLen2=0;
+				let remainingPacketLen=0;
+				let readingHeader=true;
+				let skip=false;
+				client.setOutputBufferLength=function(bufferLen)
+				{
+					//len=remainingLen+remainingPacketLen
+					assertit(remainingLen2===0);
+					assertit(remainingPacketLen===0)
+					remainingLen2=bufferLen;
+					remainingPacketLen=0;
+				}
+				return {
+					decoder(msg)
+					{
+						msg=Buffer.concat([cache,msg]);
+						let out=Buffer.alloc(0);
+						while(true)
+						{
+							if (msg.length===0)
+								return out;
+
+							if (readingHeader)
+							{
+								if (msg.length<5)
+								{
+									cache=msg;
+									return out
+								}
+								assertit([0x14,0x17].includes(msg[0]));
+								assertit(msg[1]===0x03);
+								assertit(msg[2]===0x03);
+								
+								skip=(msg.readUInt8()===0x14);
+								remainingLen1=msg.readUInt16BE(3);
+								msg=msg.slice(5);
+								readingHeader=false;
+							}
+							if (remainingLen1<=msg.length)
+							{
+								if (!skip)
+									out=Buffer.concat([out,msg.slice(0,remainingLen1)]);
+								msg=msg.slice(remainingLen1);
+								readingHeader=true;
+							}
+							else
+							{
+								remainingLen1-=msg.length;
+								if (!skip)
+									out=Buffer.concat([out,msg]);
+								return out;
+							}
+						}
+					},
+					encoder(msg)
+					{
+						let out=Buffer.alloc(0);
+						while (msg.length>0)
+						{
+							if (remainingLen2+remainingPacketLen===0)
+								throw new Error('No remaining Len');
+							if (remainingPacketLen===0)
+							{
+								let p=Math.min(remainingLen2,MAX_CHUNK_SIZE);
+								remainingLen2-=p;
+								remainingPacketLen+=p;
+								let header=Buffer.from('1703030000','hex');
+								header.writeUInt16BE(p,3);
+								out=Buffer.concat([out,header]);
+							}
+							let q=Math.min(msg.length,remainingPacketLen);
+							out=Buffer.concat([out,msg.slice(0,q)]);
+							remainingPacketLen-=q;
+							msg=msg.slice(q);
+						}
+						return out
+					}
+				}
+			})());
+		h1=await client.bufferedReadExactly(64);
+	}
+
+	let skip = h1.slice(0,8);
+	let dec_prekey_and_iv = h1.slice(8,56);
+	let dec_proto_tag = h1.slice(56,60);
+	let dec_dcId = h1.slice(60,62);
+	let trailing = h1.slice(62,64);
 
 	let enc_prekey_and_iv = Buffer.from(dec_prekey_and_iv).reverse();
 	let dec = {prekey : dec_prekey_and_iv.slice(0,32),iv : dec_prekey_and_iv.slice(32)}
 	let enc = {prekey : enc_prekey_and_iv.slice(0,32),iv : enc_prekey_and_iv.slice(32)}
 	let decoder,encoder;
 	let permission=false;
-	for (let secret in secrets)
+	let s=isTLS?[currentSecret]:secured_secrets;
+	for (let secret of s)
 	{
-		dec.key = crypto.createHash('sha256').update(Buffer.concat([dec.prekey, secret])).digest();
-		enc.key = crypto.createHash('sha256').update(Buffer.concat([enc.prekey, secret])).digest();
+		dec.key = crypto.createHash('sha256').update(Buffer.concat([dec.prekey, secret.secret])).digest();
+		enc.key = crypto.createHash('sha256').update(Buffer.concat([enc.prekey, secret.secret])).digest();
 		decoder = createAESCTRTransform(dec,true);
 		encoder = createAESCTRTransform(enc,false);
 		decoder(skip);
@@ -439,6 +648,7 @@ async function handleClient(client)
 		if (proto_tag.compare(Buffer.from('dddddddd','hex'))!==0)
 			continue;
 		permission=true;
+		currentSecret=secret;
 		break;
 	}
 
@@ -448,8 +658,11 @@ async function handleClient(client)
 	assertit(dcId<=5);
 	assertit(dcId!==0);
 	decoder(trailing);
+	if (!isTLS)
+		AD_TAG=await filter({id,address:client.remoteAddress,port:client.remotePort,secretIndex:currentSecret.index});
 	client.addCryptoLayer({decoder,encoder})
 	let server=await getFromPool(dcId);
+	server.setNoDelay();
 	client.once('finished',function(err)
 	{
 		server.emit('finished',err);
@@ -458,7 +671,21 @@ async function handleClient(client)
 	{
 		client.emit('finished',err);
 	})
+
+	server.autoWrite=false;
+	client.autoWrite=false;
+	server.idling=function()
+	{
+		client.flushBuffer();
+	}
+	client.idling=function()
+	{
+		server.flushBuffer();
+	}
+
 	let out_conn_id = crypto.randomBytes(8); //CHeck
+	client.setTimeout(CON_TIMEOUT);
+	AD_TAG=Buffer.from(AD_TAG,'hex')
 	async function serverToClient()
 	{
 		const RPC_PROXY_ANS = Buffer.from("0dda0344",'hex')
@@ -493,12 +720,16 @@ async function handleClient(client)
 		        	let conn_id = await server.bufferedReadExactly(8);
 		        	remainingLen-=8;
 		        	let padding_len = Math.floor(4*Math.random());
+		        	let msgLength=padding_len+remainingLen;
+
+
+					client.setOutputBufferLength(msgLength+4);
 		        	client.bufferedWrite((padding_len+remainingLen).toBuffer());
 		        	while(remainingLen>0)
 			        {
 			        	let data=await server.bufferedReadAtmost(remainingLen);
 			        	remainingLen-=data.length;
-			        	client.bufferedWrite(data);
+						client.bufferedWrite(data);
 			        }
 			        client.bufferedWrite(crypto.randomBytes(padding_len)); //Check
 		        }
@@ -507,6 +738,7 @@ async function handleClient(client)
 		        	let conn_id = await server.bufferedReadExactly(8);
 		        	remainingLen-=8;
 		        	let confirm = await server.bufferedReadExactly(4);
+		        	client.setOutputBufferLength(4);
 		        	client.bufferedWrite(confirm)
 		        	remainingLen-=4;
 		        }
@@ -565,16 +797,16 @@ async function handleClient(client)
 	return Promise.all([clientToServer(),serverToClient()]);
 }
 
-class MTProtoProxy extends EventEmitter 
+class MTProtoProxy
 {
 	constructor(options)
 	{
-		let {httpServer,tag,filter}=options;
-		super();
+		let {httpServer,tag,enter,leave,ready}=options;
 		let self=this;
-		secrets=options.secrets;
-		AD_TAG=tag;
-		refreshProxyInfo().then(function(){self.emit('ready')});
+		let secrets=options.secrets.map(function(secret,index){return {secret:Buffer.from(secret,'hex').slice(1),index,isTLS:secret.startsWith('ee'),isSecured:secret.startsWith('dd')}});
+		tls_secrets=secrets.filter(function(secret){return secret.isTLS});
+		secured_secrets=secrets.filter(function(secret){return secret.isSecured});
+		refreshProxyInfo().then(function(){ready()});
 		this.proxy=function(client)
 		{
 			client.on('error',function(){})
@@ -587,21 +819,18 @@ class MTProtoProxy extends EventEmitter
 					client.emit('data',data);
 					return false;
 				}
-				if (!filter)
-					filter=Promise.resolve;
+
 				let index=globaleIndex;
-				let user={id:index,address:client.remoteAddress,port:client.remotePort};
-				self.emit('connection',user);
 				globaleIndex++;
-				filter(user).then(function(){let ret= handleClient(client);client.emit('data',data); return ret}).catch(function(err)
+				handleClient(client,enter,index).catch(function(err)
 				{
-					let error=err;
+					let error=err.stack;
 					if (err.message==='client ended')
 						error=undefined;
-					self.emit('end',{id:index,bytesWritten:client.bytesWritten,bytesRead:client.bytesRead,error})
+					leave({id:index,bytesWritten:client.bytesWritten,bytesRead:client.bytesRead,error})
 					client.emit('finished',err)
 				});
-				
+				client.emit('data',data);
 			})
 		};
 	}
